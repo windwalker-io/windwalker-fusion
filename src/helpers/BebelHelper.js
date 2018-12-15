3964a3aa047ffdf0bb849759d0b8fd8eff500952
Simon Asika <asika32764@gmail.com>
2018-12-16 01:16:40 +0800

    Basic webpack

--- src/helpers/BebelHelper.js
+++ src/helpers/BebelHelper.js
@@ -4,3 +4,23 @@
  * @copyright  Copyright (C) 2018 ${ORGANIZATION}.
  * @license    __LICENSE__
  */
+
+class BabelHelper {
+  static basicOptions() {
+    return {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: 'last 3 version, safari 5, ie 10, not dead'
+          }
+        ]
+      ],
+      plugins: [
+        '@babel/plugin-proposal-class-properties'
+      ]
+    }
+  }
+}
+
+module.exports = BabelHelper;
