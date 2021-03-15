b8e9a723f3b7aa7c03ce5a322f20be6d7f921398
Simon Asika <asika32764@gmail.com>
2021-03-15 16:30:00 +0800

    wip

--- src/base/watch.js
+++ src/base/watch.js
@@ -8,21 +8,39 @@ import gulp from 'gulp';
 import { cliInput } from '../utilities/cli.js';
 import EventEmitter from 'events';
 
+const FUNC_REGEX = /at\s{1}(?<func>\w+)\s{1}\([\W\w]+?\)/g;
+
 const watches = [];
 let startWatching = false;
 
 export function watch(glob, opt, fn) {
   if (arguments.length === 1) {
-
+    const currentTask = findCurrentTask(new Error());
+    
     if (!startWatching && cliInput['watch']) {
       watches.push({
-        task: gulp.currentTask.name,
+        task: currentTask,
         glob: glob
       });
     }
+
+    console.log(watches);
 
     return new EventEmitter();
   }
 
   return gulp.watch(glob, opt, fn);
+}
+
+/**
+ * @param {Error} e
+ * @returns {string}
+ */
+function findCurrentTask(e) {
+  // Drop first
+  FUNC_REGEX.exec(e.stack);
+
+  // Get second
+  const match = FUNC_REGEX.exec(e.stack)
+  return match[1];
 }
